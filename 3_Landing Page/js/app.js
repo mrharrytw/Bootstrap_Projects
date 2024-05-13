112024d56ab1d77e9093e315ccb80f90f83499de
Harry <mrharrytw@gmail.com>
2024-05-13 19:42:57 +0630

    add css for propertylists, titles(before and after), activeitems, and add js for toggleClass to navbuttons, filter to propertylists

--- 3_Landing Page/js/app.js
+++ 3_Landing Page/js/app.js
@@ -12,5 +12,49 @@ $(document).ready(function () {
             $('.navbar').removeClass('navmenus');
         }
     });
+
+    $('.navbuttons').click(function () {
+        $(this).toggleClass('crossxs');
+    });
     // End Nav Bar
+    //-----------------------------------------------------------------------
+
+    // Start Property Section
+    $('.propertylists').click(function () {
+        $(this).addClass('activeitems').siblings().removeClass('activeitems');
+
+        // const filtervalue = $(this).attr('data-filter');
+        const filtervalue = $(this).data('filter');
+
+        if (filtervalue === 'all') {
+            $('.filters').show('slide', 1000);
+        } else {
+
+            // Method 1
+            // $('.filters').hide();
+            // $('.filters').filter('.' + filtervalue).show();
+
+            // Method 2
+            $('.filters').not('.' + filtervalue).hide('explode', 1000);
+            $('.filters').filter('.' + filtervalue).show('slide', 1000);
+        }
+    });
+    // End Property Section
+    //-----------------------------------------------------------------------
+
+    // Start Adv Section
+    $(window).scroll(function () {
+
+        let getscrolltop = $(this).scrollTop();
+        // console.log(getscrolltop);
+
+        if (getscrolltop >= 1200) {
+            $('.advimages').addClass('fromlefts');
+            $('.advtexts').addClass('fromrights');
+        } else {
+            $('.advimages').removeClass('fromlefts');
+            $('.advtexts').removeClass('fromrights');
+        }
+    });
+    // End Adv Section
 });
