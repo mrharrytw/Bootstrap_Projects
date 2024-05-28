01808e6e8dc618531330c3c81e2ca6f1b1aa635a
Harry <mrharrytw@gmail.com>
2024-05-28 22:59:20 +0630

    added things to content area and added footer

--- 4_Admin Template/js/app.js
+++ 4_Admin Template/js/app.js
@@ -1,3 +1,23 @@
+
+// Start Navbar
+
+// start top navbar
+function dropbtn(e) {
+    // console.log(e.target.parentElement.nextElementSibling);
+    e.target.parentElement.nextElementSibling.classList.toggle("show")
+}
+// end top navbar
+
+// End Navbar
+
 $(document).ready(function () {
 
+    // Start Navbar
+    // start left sidebar
+    $(".sidebarlinks").click(function () {
+        $('.sidebarlinks').removeClass('currents');
+        $(this).addClass('currents');
+    });
+    // end left sidebar
+    // End Navbar
 });
