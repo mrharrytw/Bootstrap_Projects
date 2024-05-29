1eb7dc81c1cc5225fc79afa00275b62cde692c2f
Harry <mrharrytw@gmail.com>
2024-05-29 20:36:45 +0630

    added  divs for  site setting  and right panels for changgning theme color

--- 4_Admin Template/js/app.js
+++ 4_Admin Template/js/app.js
@@ -1,8 +1,16 @@
+// Start Right Navbar
+const getsitesettings = document.getElementById('sitesettings');
+
+getsitesettings.addEventListener('click', function () {
+    document.body.classList.toggle('show-nav')
+});
+// End Right Navbar
 
 // Start Navbar
 
-// start top navbar
+// start Top navbar
 function dropbtn(e) {
+    // console.log(e.target);
     // console.log(e.target.parentElement.nextElementSibling);
     e.target.parentElement.nextElementSibling.classList.toggle("show")
 }
@@ -13,11 +21,27 @@ function dropbtn(e) {
 $(document).ready(function () {
 
     // Start Navbar
+
+    // start right Navbar
+    // $(".sitesettings").click(function () {
+    //     $('body').toggleClass('show-nav')
+    // });
+    // end right Navbar
+
     // start left sidebar
     $(".sidebarlinks").click(function () {
         $('.sidebarlinks').removeClass('currents');
         $(this).addClass('currents');
     });
     // end left sidebar
+
     // End Navbar
+
+
+
+    // Start Footer
+    const getyear = document.getElementById("getyear");
+    const getfullyear = new Date().getFullYear();
+    getyear.textContent = getfullyear;
+    // End Footer
 });
